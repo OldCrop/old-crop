e78f11bc44bff4a07f9650d5efa13c8d9aa326f1
Yyyeiow <yyxxnuuu@catholic.ac.kr>
2024-09-28 15:20:00 +0900

    Main_Intro, Intro, Main_Menu 변경

--- Folktale/Phase_Main_Intro.h
+++ Folktale/Phase_Main_Intro.h
@@ -20,8 +20,23 @@ private:
 	SDL_Rect source_rectangle_; // the rectangle for source image
 	SDL_Rect destination_rectangle_; // for destination
 	
-	SDL_Texture* text_texture_;
-	SDL_Rect text_rect_;
+	SDL_Texture* face_texture_;
+	SDL_Rect face_rect_;
+
+	SDL_Texture* expression_texture_;
+	SDL_Rect expression_rect_;
+
+	//SDL_Texture* text_texture_;
+	//SDL_Rect text_rect_;
+
+	// 더블 클릭 감지 변수
+	int click_count_;
+	Uint32 last_click_time_;
+	bool show_face_and_expression_;
+	Uint32 show_start_time_; // 이미지를 보여주기 시작한 시간
+
+	//게임 처음 실행 여부
+	bool is_first_click_;
 };
 
 class Intro : public PhaseInterface
